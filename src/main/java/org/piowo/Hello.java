324e42e4949057926ac0513afefc1aa5888345bd
piowo <piowo1@o2.pl>
2022-04-13 23:00:10 +0200

    Initial commit

---
+++ src/main/java/org/piowo/Hello.java
@@ -0,0 +1,15 @@
+package org.piowo;
+
+public class Hello {
+    private String imie;
+    public Hello() {
+        imie = "Piotrek!";
+    }
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+}
